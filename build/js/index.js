1ec04a026d1f33623551225ae8cf9af30c824b54
cleve-keem <belloharyourmidey@gmail.com>
2024-04-29 04:09:53 -0700

    uploading

--- build/js/index.js
+++ build/js/index.js
@@ -1,6 +1,6 @@
-const navMenu = document.querySelector('mobile-menu');
-const closeMenu = document.querySelector('mobile-close-menu');
-const openMenuIcon = document.querySelector('mobile-close-menu-icon');
+const navMenu = document.getElementById('mobile-menu');
+const closeMenu = document.getElementById('mobile-close-menu');
+const openMenuIcon = document.getElementById('mobile-close-menu-icon');
 
 const openNavClass = 'left-0';
 const closeNavClass = '-left-[300px]';
@@ -9,9 +9,18 @@ const closeNavIcon = 'ri-arrow-left-s-line';
 
 closeMenu.addEventListener('click', ()=>{
     if(navMenu.classList.contains(closeNavClass)){
-        navMenu.classList.toggle(openNavClass);
+        navMenu.classList.remove(closeNavClass);
+        navMenu.classList.add(openNavClass);
 
-        openMenuIcon.classList.toggle(openNavIcon);
-        openMenuIcon.classList.toggle(openNavIcon);
+        openMenuIcon.classList.remove(openNavIcon);
+        openMenuIcon.classList.add(closeNavIcon);
+    }
+    else{
+        navMenu.classList.remove(openNavClass);
+        navMenu.classList.add(closeNavClass);
+
+        openMenuIcon.classList.remove(closeNavIcon);
+        openMenuIcon.classList.add(openNavIcon);
+
     }
 });
