98beed967017f389b11796553a328ff4710aff05
cleve-keem <belloharyourmidey@gmail.com>
2024-05-24 03:40:29 -0700

    uploading testimonials

--- build/js/index.js
+++ build/js/index.js
@@ -24,3 +24,12 @@ closeMenu.addEventListener('click', ()=>{
 
     }
 });
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
